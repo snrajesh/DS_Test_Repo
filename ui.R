d471c812a0af6c269dcb1bf120a06bb32cd4ca5d
Rajesh Santha Nambiar <sn_rajesh@hotmail.com>
2015-11-21 20:07:04 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,30 +1,38 @@
 # ui.R
 
 library(shiny)
-#library(plotly)
-states <- readRDS('states.rds')
+library(plotly)
+library(rCharts)
+
+#load state code for the drop-down list
+states <- readRDS('./data/states.rds')
+
 
 # UI for the application 
 shinyUI(fluidPage(#theme = "bootstrap.css",
     # Application title
     titlePanel("PM25 Emissions Analysis"),
-    sidebarLayout( #position = "left",
-        # Sidebar with Instruction and user input fields 
-        sidebarPanel(
+    #sidebar
+    sidebarLayout( #position = "left", fluid = TRUE,
+        # Sidebar with Instructions and user input fields 
+        sidebarPanel( width = 4,
             h4("What?", style = "color: steelblue"),
-            p("This application provides a comparative overview of Fine particle (PM25) emissions across USA."),
+            p("This application provides a comparative overview of fine particle (PM2.5) emissions",
+                "in the United States."),
             h4("How?", style = "color: steelblue"),
-            #p("Click ",  strong("How do I use this?", style = "color: darkgreen"), " tab to learn how to use this."),
+            p("Click the ", strong("Documentation", style = "color: darkgreen"), 
+              " tab to learn how to use this."),
             #p("Visit the ", strong("What is the receipe?", style = "color: darkgreen"), " tab to see how it is made."),
-            p("Click the ", strong("Documentation", style = "color: darkgreen"), " tab to learn how to use this."),
             h4("Ready to try?", style = "color: steelblue"),
+            # display US map with states shaded based on emission
             plotOutput("USmap"), #, width = "100%", height = "400px"), #plotlyOutput("myPlot3"),
             helpText("Want to see the details of your state?"),
             selectInput('stateCode', 'Select a State from below:', states$stateCode),
             #submitButton('Submit'),
+            # display the state map with counties shaded based on emission
             plotOutput("stateMap")
         ),
-        # Show a plot of the generated distribution
+        # main panel with plots (and prediction) generated based on user input
         mainPanel(
             tabsetPanel( # position = "below",
                 tabPanel("Emissions Analysis", 
@@ -32,46 +40,23 @@ shinyUI(fluidPage(#theme = "bootstrap.css",
                          plotOutput('trendPlot'),
                          br(),
                          h4('Emissions By County:'),
-                         plotlyOutput("myPlot3"),
+                         plotlyOutput("countyPlot"),
                          br(),
-                         #showOutput("myPlot5","nvd3"),
-                         br(),
+                         #showOutput("barPlot"),#,"nvd3"),
+                         htmlOutput("barPlot"),
+                         #chartOutput("barPlot"),
+                         #br(),
                          h4('Liner Regression Model: '),
                          verbatimTextOutput("model")
                 ),
                 #tabPanel("Emissions in all states",dataTableOutput("stateTable")),
                 tabPanel("Emissions Data",dataTableOutput("countyTable")),
                 tabPanel("Documentation", #"How do I use this?", 
-                         h5("This application provides a comparative overview of Fine particle (PM25) emissions across USA.")
+                         h5("This application provides a comparative overview of fine particle (PM25) emissions across USA.")
                 ),
-                tabPanel("What is the receipe?",includeMarkdown("README.md"))
+                tabPanel("What is the receipe?",includeMarkdown("prompt.Rmd"))
             )
         )
     )
 ))
 
-# shinyUI(pageWithSidebar(
-#     # Application title
-#     headerPanel("PM25 Emissions"),
-#     # Sidebar with user input fields 
-#     sidebarPanel(
-#         #textInput(inputId="stateCode", label = "Enter State Code", value = "NY"),
-#         selectInput('stateCode', 'Select State', states$stateCode),
-#         #submitButton('Submit'),
-#         #
-#         h5('You entered'),
-#         verbatimTextOutput("inputState"),
-#         h5('Which resulted in a prediction of '),
-#         verbatimTextOutput("prediction")
-#     ),
-#     # Show a plot of the generated distribution
-#     mainPanel(
-#         h3('Results of prediction'),
-#         #
-#         h3('Plot of Emissions:'),
-#         plotOutput('myPlot'),
-#         plotOutput('myPlot2'),
-#         h3('Liner Regression Model: '),
-#         verbatimTextOutput("model")
-#     )
-# ))
